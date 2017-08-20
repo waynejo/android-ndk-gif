ebd22adf2d8dd369479e6b5135aed5d5a704e3bf
Wayne <waynejo1024@gmail.com>
2017-08-20 17:11:20 +0900

    Creates an exception when encoding an image of an image different from the size initially created.

--- library/androidndkgif/src/com/waynejo/androidndkgif/GifEncoder.java
+++ library/androidndkgif/src/com/waynejo/androidndkgif/GifEncoder.java
@@ -3,6 +3,7 @@ package com.waynejo.androidndkgif;
 import android.graphics.Bitmap;
 
 import java.io.FileNotFoundException;
+import java.util.Locale;
 
 public class GifEncoder {
 
@@ -27,6 +28,9 @@ public class GifEncoder {
     private long instance = 0;
     private int threadCount = 1;
 
+    private int width;
+    private int height;
+
     public void init(int width, int height, String path) throws FileNotFoundException {
         init(width, height, path, EncodingType.ENCODING_TYPE_NORMAL_LOW_MEMORY);
     }
@@ -35,6 +39,8 @@ public class GifEncoder {
         if (0 != instance) {
             close();
         }
+        this.width = width;
+        this.height = height;
         instance = nativeInit(width, height, path, encodingType.ordinal(), threadCount);
         if (0 == instance) {
             throw new FileNotFoundException();
@@ -64,6 +70,10 @@ public class GifEncoder {
     public boolean encodeFrame(Bitmap bitmap, int delayMs) {
         if (0 == instance) {
             return false;
+        }
+        if (bitmap.getWidth() != width || bitmap.getHeight() != height) {
+            String errorMessage = String.format(Locale.ENGLISH, "The size specified at initialization differs from the size of the image.\n expected:(%d, %d) actual:(%d,%d)", width, height, bitmap.getWidth(), bitmap.getHeight());
+            throw new RuntimeException(errorMessage);
         }
         nativeEncodeFrame(instance, bitmap, delayMs);
         return true;
